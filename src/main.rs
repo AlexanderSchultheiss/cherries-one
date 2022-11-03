a51792eb2dc04466bf3acbf2608225c76785e9cd
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-11-03 15:52:59 +0100

    feat: added counting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
     println!("Hello, world!");
 
+    let x = 0;
+
     for i in 1..10 {
         println!("At {}", i);
+        x += i;
+    };
+
+
+    if x > 10 {
+        println!("So much!");
     }
 }
