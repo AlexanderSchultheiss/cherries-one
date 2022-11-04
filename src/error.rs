9cfb357e0940ad88afa9e2e851e003c73468ec68
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-11-04 14:27:46 +0100

    feat: implemented Display for Error

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,10 @@
+use std::fmt::{Display, Formatter};
 
+#[derive(Debug, Clone)]
 pub struct Error(String);
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
